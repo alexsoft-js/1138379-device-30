a657a080cb41afb03d1041c4dad049a04424e6c6
alexsoft.js <alexsoft.js@gmail.com>
2021-02-01 16:20:42 +0300

    Графика всех страниц

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 // header-dropdown start
-let headerServicesLinkCatalog = document.querySelector('.header-services__link--catalog');
+  let headerServicesLinkCatalog = document.querySelector('.header-services__link--catalog');
   let headerDropdown = document.querySelector('.header-dropdown');
   let headerDropdownFlex = document.querySelector('.header-dropdown--flex');
 
-  headerServicesLinkCatalog.onclick = function() {
-    headerDropdown.classList.toggle('header-dropdown--flex');
-  } 
+    headerServicesLinkCatalog.onclick = function() {
+      headerDropdown.classList.toggle('header-dropdown--flex');
+    } 
 // header-dropdown end
